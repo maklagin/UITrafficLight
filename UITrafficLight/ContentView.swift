344e52b33e27e0c40b910d0777df286732970426
maklagin <108239343+maklagin@users.noreply.github.com>
2022-09-08 16:53:27 +0300

    Initial Commit

---
+++ UITrafficLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITrafficLight
+//
+//  Created by Максим Кулагин on 08.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITrafficLight/UITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITrafficLightApp.swift
+//  UITrafficLight
+//
+//  Created by Максим Кулагин on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct UITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
